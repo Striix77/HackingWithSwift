216a09964bffcb6e3d0ca59e5ebef1277ee19cd0
Erik <muzsi.erik@agilefreaks.com>
2026-03-16 16:10:23 +0200

    Created a basic UI with no functionality (just yet): Two text views and 3 buttons with their corresponding flag images as labels

---
+++ Challenge 1/ToLengths/ToLengths/ToLengthsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToLengthsApp.swift
+//  ToLengths
+//
+//  Created by Freak on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToLengthsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
