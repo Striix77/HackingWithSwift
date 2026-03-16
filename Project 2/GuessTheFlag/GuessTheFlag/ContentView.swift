49a84ccd8802c68334ef435041b97d4d3972b5ac
Erik <muzsi.erik@agilefreaks.com>
2026-03-16 16:28:25 +0200

    Improved the design: - Added a prettier background gradient - Improved title fonts - Improved buttons (clipShape and shadow)

--- Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -33,14 +33,20 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
+            LinearGradient(
+                colors: [.blue, .black],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
 
                 ForEach(0..<3) { number in
@@ -48,6 +54,8 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
 
