570d0486ca5d874bbf281fb69cb4a06dc7be015e
Erik <muzsi.erik@agilefreaks.com>
2026-03-16 11:38:50 +0200

    - Created dev branch and added Project 1 to it

---
+++ Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Freak on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
