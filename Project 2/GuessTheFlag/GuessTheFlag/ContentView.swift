61ff370596ab7bc4a1ee6e86929757d13c43b3c5
Erik <muzsi.erik@agilefreaks.com>
2026-03-17 13:27:59 +0200

    - Replaced Image fro the flags with custom view FlagImage (part of Project 3)

--- Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    let imgURL: String
+    
+    var body: some View {
+        Image(imgURL)
+            .clipShape(.capsule)
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = [
         "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland",
@@ -106,9 +116,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .clipShape(.capsule)
-                                .shadow(radius: 5)
+                            FlagImage(imgURL: countries[number])
                         }
                     }
 
