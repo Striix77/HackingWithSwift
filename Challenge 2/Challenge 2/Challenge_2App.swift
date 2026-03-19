9aec639ea2786074b44529768d31790010e8dd09
Erik M <muzsi.erik@agilefreaks.com>
2026-03-19 13:53:09 +0200

    Create empty project

---
+++ Challenge 2/Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by Freak on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 2/Challenge 2Tests/Challenge_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2Tests.swift
+//  Challenge 2Tests
+//
+//  Created by Freak on 19.03.2026.
+//
+
+import Testing
+@testable import Challenge_2
+
+struct Challenge_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
