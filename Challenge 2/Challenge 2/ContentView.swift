b05d28c5657315217638dcc4ef1ae85a346a73ef
Erik M <muzsi.erik@agilefreaks.com>
2026-03-19 15:48:31 +0200

    Add score number to the title and color changing prompt
    Score number now properly shows the player's score.
    The win/lose prompt now changes colors accordingly.

--- Challenge 2/Challenge 2/ContentView.swift
+++ Challenge 2/Challenge 2/ContentView.swift
@@ -37,25 +37,34 @@ enum Moves: String, CaseIterable {
 struct ContentView: View {
     @State private var currentMove: Moves = .random()
     @State private var shouldWin: Bool = Bool.random()
+    @State private var score: Int = 0 {
+        didSet{
+            if score < 0 {
+                score = 0
+            }
+        }
+    }
     @State private var showAlert: Bool = false
     @State private var alertTitle: String = ""
     @State private var alertDetails: String = ""
 
     func processPlayerChoice(_ playerMove: Moves) {
-        if (shouldWin && playerMove.beats(currentMove)) || (!shouldWin && !playerMove.beats(currentMove)) {
-            print("win")
+        if (shouldWin && playerMove.beats(currentMove))
+            || (!shouldWin && !playerMove.beats(currentMove))
+        {
             alertTitle = "You Win!"
             alertDetails = "Nice job! You sure showed the randomizer!"
-        }
-        else {
-            print("lose")
+            score += 10
+        } else {
             alertTitle = "You Lose!"
             alertDetails = "Well well...\nBetter luck next time champ!"
+            score -= 10
+            
         }
         showAlert = true
-        
+
     }
-    
+
     func resetGame() {
         currentMove = .random()
         shouldWin = Bool.random()
@@ -69,12 +78,31 @@ struct ContentView: View {
                     Color(red: 0.1, green: 0.6, blue: 0.75, opacity: 1)
                         .ignoresSafeArea()
                     VStack {
-                        Text("Score: ???")
+                        Text("Score: \(score)")
                             .font(.largeTitle)
                         Text("Current move: \(currentMove.emoji)")
                             .font(.title)
-                        Text("You should try to \(shouldWin ? "win" : "lose")")
-                            .font(.title)
+                        HStack {
+                            Text("You should try to")
+                                .font(.title)
+                            Text("\(shouldWin ? "win" : "lose")")
+                                .font(.title)
+                                .foregroundStyle(
+                                    shouldWin
+                                        ? Color(
+                                            red: 0.4,
+                                            green: 0.9,
+                                            blue: 0.4,
+                                            opacity: 1
+                                        )
+                                        : Color(
+                                            red: 0.5,
+                                            green: 0.2,
+                                            blue: 0.2,
+                                            opacity: 1
+                                        )
+                                )
+                        }
 
                         Spacer()
 
@@ -101,8 +129,8 @@ struct ContentView: View {
             .alert(
                 alertTitle,
                 isPresented: $showAlert
-            ){
-                Button("Continue"){
+            ) {
+                Button("Continue") {
                     resetGame()
                 }
             } message: {
