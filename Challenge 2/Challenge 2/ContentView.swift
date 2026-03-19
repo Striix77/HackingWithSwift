c7f8a52ec9134c58db93326c07798b59dd8f12f3
Erik M <muzsi.erik@agilefreaks.com>
2026-03-19 15:37:52 +0200

    Add functionality to the buttons for a simple game loop
    By clicking on the buttons, the player can choose a move and based on that they either win or lose.
    
    After each round an alert shows up with the win/loss info, the player can play a new round by clicking continue.

--- Challenge 2/Challenge 2/ContentView.swift
+++ Challenge 2/Challenge 2/ContentView.swift
@@ -21,31 +21,68 @@ enum Moves: String, CaseIterable {
         case .scissors: return "✂️"
         }
     }
+
+    func beats(_ move: Moves) -> Bool {
+        switch self {
+        case .rock:
+            return move != Moves.rock && move != Moves.paper
+        case .paper:
+            return move != Moves.paper && move != Moves.scissors
+        case .scissors:
+            return move != Moves.scissors && move != Moves.rock
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var currentChoice: Moves = .random()
+    @State private var currentMove: Moves = .random()
     @State private var shouldWin: Bool = Bool.random()
+    @State private var showAlert: Bool = false
+    @State private var alertTitle: String = ""
+    @State private var alertDetails: String = ""
+
+    func processPlayerChoice(_ playerMove: Moves) {
+        if (shouldWin && playerMove.beats(currentMove)) || (!shouldWin && !playerMove.beats(currentMove)) {
+            print("win")
+            alertTitle = "You Win!"
+            alertDetails = "Nice job! You sure showed the randomizer!"
+        }
+        else {
+            print("lose")
+            alertTitle = "You Lose!"
+            alertDetails = "Well well...\nBetter luck next time champ!"
+        }
+        showAlert = true
+        
+    }
+    
+    func resetGame() {
+        currentMove = .random()
+        shouldWin = Bool.random()
+        showAlert = false
+    }
+
     var body: some View {
         NavigationView {
             VStack {
-                ZStack{
-                    Color(red: 0.1, green: 0.6, blue: 0.75, opacity: 1).ignoresSafeArea()
+                ZStack {
+                    Color(red: 0.1, green: 0.6, blue: 0.75, opacity: 1)
+                        .ignoresSafeArea()
                     VStack {
                         Text("Score: ???")
                             .font(.largeTitle)
-                        Text("Current move: ???")
+                        Text("Current move: \(currentMove.emoji)")
                             .font(.title)
                         Text("You should try to \(shouldWin ? "win" : "lose")")
                             .font(.title)
-                        
+
                         Spacer()
-                        
+
                         VStack {
                             HStack {
                                 ForEach(Moves.allCases, id: \.self) { move in
                                     Button(move.emoji) {
-                                        
+                                        processPlayerChoice(move)
                                     }
                                     .buttonStyle(.bordered)
                                     .font(.system(size: 70))
@@ -53,13 +90,24 @@ struct ContentView: View {
                             }
                         }
                         Spacer()
-                        
+
                     }
+
                 }
 
             }
             .navigationTitle("Guided RPS")
             .navigationBarTitleDisplayMode(.inline)
+            .alert(
+                alertTitle,
+                isPresented: $showAlert
+            ){
+                Button("Continue"){
+                    resetGame()
+                }
+            } message: {
+                Text(alertDetails)
+            }
         }
     }
 }
