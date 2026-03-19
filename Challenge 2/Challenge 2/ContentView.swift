eaf5a55e7a198c922e862baf06230b2bbdc7b0ce
Erik M <muzsi.erik@agilefreaks.com>
2026-03-19 14:55:02 +0200

    Create basic app design
    Add basic UI required for core game loop:
    - Add text views to display the current score and the apps move
    - Display the win/lose target condition for the next round
    - Add 3 interactive buttons for the player's choice

--- Challenge 2/Challenge 2/ContentView.swift
+++ Challenge 2/Challenge 2/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum Moves: String, CaseIterable {
+    case rock, paper, scissors
+
+    static func random() -> Moves {
+        return allCases.randomElement() ?? .rock
+    }
+
+    var emoji: String {
+        switch self {
+        case .rock: return "🪨"
+        case .paper: return "📄"
+        case .scissors: return "✂️"
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var currentChoice: Moves = .random()
+    @State private var shouldWin: Bool = Bool.random()
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack{
+                    Color(red: 0.1, green: 0.6, blue: 0.75, opacity: 1).ignoresSafeArea()
+                    VStack {
+                        Text("Score: ???")
+                            .font(.largeTitle)
+                        Text("Current move: ???")
+                            .font(.title)
+                        Text("You should try to \(shouldWin ? "win" : "lose")")
+                            .font(.title)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            HStack {
+                                ForEach(Moves.allCases, id: \.self) { move in
+                                    Button(move.emoji) {
+                                        
+                                    }
+                                    .buttonStyle(.bordered)
+                                    .font(.system(size: 70))
+                                }
+                            }
+                        }
+                        Spacer()
+                        
+                    }
+                }
+
+            }
+            .navigationTitle("Guided RPS")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
