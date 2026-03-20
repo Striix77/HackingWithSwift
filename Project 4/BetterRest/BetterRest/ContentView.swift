70c5250136a1d1b4e51d97ea3902d6177190b27b
Erik M <muzsi.erik@agilefreaks.com>
2026-03-20 11:28:24 +0200

    Add files that were not staged in the last commit

--- Project 4/BetterRest/BetterRest/ContentView.swift
+++ Project 4/BetterRest/BetterRest/ContentView.swift
@@ -8,16 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount: Double = 8.0
+    @State private var wakeUp = Date.now
+
+    let components = Calendar.current.dateComponents(
+        [.hour, .minute],
+        from: Date.now
+    )
+    var hour: Int { components.hour ?? 0 }
+    var minute: Int { components.minute ?? 0 }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper(
+                "\(sleepAmount.formatted()) hours",
+                value: $sleepAmount,
+                in: 0...16,
+                step: 0.25
+            )
+            DatePicker("Please enter a date", selection: $wakeUp)
+                .labelsHidden()
+            DatePicker(
+                "Please enter a date",
+                selection: $wakeUp,
+                displayedComponents: .hourAndMinute
+            )
+            .labelsHidden()
+            DatePicker(
+                "Please enter a date",
+                selection: $wakeUp,
+                in: Date.now...
+            )
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
         .padding()
     }
 }
+
+//func exampleDates() {
+//    let tomorrow = Date.now.addingTimeInterval(86400)
+//    let range = Date.now...tomorrow
+//}
 
 #Preview {
     ContentView()
