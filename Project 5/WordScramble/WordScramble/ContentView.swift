8a4d302e8391549828ef3d93b7e4e6faf4e4e744
Erik M <muzsi.erik@agilefreaks.com>
2026-03-23 14:53:40 +0200

    feat: Implement input validation

--- Project 5/WordScramble/WordScramble/ContentView.swift
+++ Project 5/WordScramble/WordScramble/ContentView.swift
@@ -57,6 +57,32 @@ struct ContentView: View {
         }
         newWord = ""
     }
+    
+    func isOriginal(_ word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(_ word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(_ word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location:0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
 }
 
 #Preview {
