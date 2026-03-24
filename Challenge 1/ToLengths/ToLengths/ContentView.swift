6358bb7984a12bf28330c957d3f26b6b9d78548d
Erik M <muzsi.erik@agilefreaks.com>
2026-03-24 10:41:35 +0200

    refactor: Extract the two sections into different view structs

--- Challenge 1/ToLengths/ToLengths/ContentView.swift
+++ Challenge 1/ToLengths/ToLengths/ContentView.swift
@@ -7,11 +7,57 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
     @State var inputLength = 0.0
     @State var inputUnitOfConversion: Units = Units.meters
     @State var outputUnitOfConversion: Units = Units.kilometers
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                UnitSelectionView(
+                    inputUnitOfConversion: $inputUnitOfConversion,
+                    outputUnitOfConversion: $outputUnitOfConversion
+                )
+
+                ConversionInputView(
+                    inputLength: $inputLength,
+                    inputUnitOfConversion: $inputUnitOfConversion,
+                    outputUnitOfConversion: $outputUnitOfConversion
+                )
+
+            }
+            .navigationTitle("ToLengths")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct UnitSelectionView: View {
+    @Binding var inputUnitOfConversion: Units
+    @Binding var outputUnitOfConversion: Units
+    var body: some View {
+        Section("Choose your units of measure") {
+
+            Picker("Convert from", selection: $inputUnitOfConversion) {
+                ForEach(Units.allCases, id: \.self) { unit in
+                    Text(String(describing: unit).capitalized)
+                }
+            }
+
+            Picker("Convert to", selection: $outputUnitOfConversion) {
+                ForEach(Units.allCases, id: \.self) { unit in
+                    Text(String(describing: unit).capitalized)
+                }
+            }
+        }
+    }
+}
+
+struct ConversionInputView: View {
+    @Binding var inputLength: Double
+    @Binding var inputUnitOfConversion: Units
+    @Binding var outputUnitOfConversion: Units
 
     var outputLength: Double {
 
@@ -21,43 +67,23 @@ struct ContentView: View {
     }
 
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Choose your units of measure") {
+        Section("Enter the value to be converted") {
+            HStack {
+                TextField("", value: $inputLength, format: .number)
+                    .fixedSize()
+                    .keyboardType(.numbersAndPunctuation)
+                Text(inputUnitOfConversion.unitSuffix)
+                    .offset(x: -5)
+                Spacer()
+                Text(
+                    outputLength,
+                    format: .number.precision(.fractionLength(0...4))
+                )
+                Text(outputUnitOfConversion.unitSuffix)
+                    .offset(x: -5)
 
-                    Picker("Convert from", selection: $inputUnitOfConversion) {
-                        ForEach(Units.allCases, id: \.self) { unit in
-                            Text(String(describing: unit).capitalized)
-                        }
-                    }
-
-                    Picker("Convert to", selection: $outputUnitOfConversion) {
-                        ForEach(Units.allCases, id: \.self) { unit in
-                            Text(String(describing: unit).capitalized)
-                        }
-                    }
-                }
-                Section("Enter the value to be converted") {
-                    HStack {
-                        TextField("", value: $inputLength, format: .number)
-                            .fixedSize()
-                            .keyboardType(.numbersAndPunctuation)
-                        Text(inputUnitOfConversion.unitSuffix)
-                            .offset(x: -5)
-                        Spacer()
-                        Text(
-                            outputLength,
-                            format: .number.precision(.fractionLength(0...4))
-                        )
-                        Text(outputUnitOfConversion.unitSuffix)
-                            .offset(x: -5)
-
-                    }
-
-                }
             }
-            .navigationTitle("ToLengths")
-            .navigationBarTitleDisplayMode(.inline)
+
         }
     }
 }
