49759d7a607fe9ece06a04fb198ea9bfd2ac9bd3
Erik M <muzsi.erik@agilefreaks.com>
2026-03-25 16:46:39 +0200

    Wrap body in scroll view

--- Project 6/Animations/Animations/ContentView.swift
+++ Project 6/Animations/Animations/ContentView.swift
@@ -15,94 +15,97 @@ struct ContentView: View {
     @State private var overlayAnimationAmount = 1.0
     @State private var stepperAnimationAmount = 1.0
     var body: some View {
-        Button("Tap me") {
-            animationAmount += 0.5
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
-
-        Button("Spring me") {
-            springAnimationAmount += 0.5
-        }
-        .padding(50)
-        .background(.yellow)
-        .foregroundStyle(.black)
-        .clipShape(.circle)
-        .scaleEffect(springAnimationAmount)
-        .animation(
-            .spring(duration: 0.5, bounce: 0.3),
-            value: springAnimationAmount
-        )
-
-        Button("Ease me") {
-            easeInOutAnimationAmount += 0.5
-        }
-        .padding(50)
-        .background(.blue)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(easeInOutAnimationAmount)
-        .animation(
-            .easeInOut(duration: 1)
-                .delay(1),
-            value: easeInOutAnimationAmount
-        )
-        Button("Repeat me") {
-            repeatAnimationAmount = 1.5
-        }
-        .padding(50)
-        .background(.green)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(repeatAnimationAmount)
-        .animation(
-            .easeInOut(duration: 1)
-                .repeatForever(autoreverses: true),
-            value: repeatAnimationAmount
-        )
-
-        Button("Pulse me") {
-            overlayAnimationAmount = 2
-        }
-        .padding(50)
-        .background(.cyan)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.cyan)
-                .scaleEffect(overlayAnimationAmount)
-                .opacity(2 - overlayAnimationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: overlayAnimationAmount
-                )
-        )
-
-        VStack {
-            Stepper("Scale amount", value: $stepperAnimationAmount.animation(
-                .easeInOut(duration: 0.5)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-
-            Spacer()
-
-            Button("Tap Me") {
-                stepperAnimationAmount += 0.5
+        ScrollView{
+            Button("Tap me") {
+                animationAmount += 0.5
             }
-            .padding(40)
+            .padding(50)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(.circle)
-            .scaleEffect(stepperAnimationAmount)
+            .scaleEffect(animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
+            .animation(.default, value: animationAmount)
+            
+            Button("Spring me") {
+                springAnimationAmount += 0.5
+            }
+            .padding(50)
+            .background(.yellow)
+            .foregroundStyle(.black)
+            .clipShape(.circle)
+            .scaleEffect(springAnimationAmount)
+            .animation(
+                .spring(duration: 0.5, bounce: 0.3),
+                value: springAnimationAmount
+            )
+            
+            Button("Ease me") {
+                easeInOutAnimationAmount += 0.5
+            }
+            .padding(50)
+            .background(.blue)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(easeInOutAnimationAmount)
+            .animation(
+                .easeInOut(duration: 1)
+                .delay(1),
+                value: easeInOutAnimationAmount
+            )
+            Button("Repeat me") {
+                repeatAnimationAmount = 1.5
+            }
+            .padding(50)
+            .background(.green)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(repeatAnimationAmount)
+            .animation(
+                .easeInOut(duration: 1)
+                .repeatForever(autoreverses: true),
+                value: repeatAnimationAmount
+            )
+            
+            Button("Pulse me") {
+                overlayAnimationAmount = 2
+            }
+            .padding(50)
+            .background(.cyan)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.cyan)
+                    .scaleEffect(overlayAnimationAmount)
+                    .opacity(2 - overlayAnimationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: overlayAnimationAmount
+                    )
+            )
+            
+            VStack {
+                Stepper("Scale amount", value: $stepperAnimationAmount.animation(
+                    .easeInOut(duration: 0.5)
+                    .repeatCount(3, autoreverses: true)
+                ), in: 1...10)
+                
+                Spacer()
+                
+                Button("Tap Me") {
+                    stepperAnimationAmount += 0.5
+                }
+                .padding(40)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(stepperAnimationAmount)
+            }
+            
+            
         }
-
     }
 }
 
