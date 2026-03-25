5afed6230bf754e66001d69ee872b3b083e1e2d6
Erik M <muzsi.erik@agilefreaks.com>
2026-03-25 13:19:51 +0200

    Create test for correct bedtime prediction

--- Project 4/BetterRest/BetterRestTests/BetterRestTests.swift
+++ Project 4/BetterRest/BetterRestTests/BetterRestTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 @testable import BetterRest
+import Foundation
 
 struct BetterRestTests {
 
@@ -20,6 +21,21 @@ struct BetterRestTests {
         #expect(components.hour == 7)
         #expect(components.minute == 0)
     }
+    
+    @MainActor
+    @Test("CoreML successfully predicts bedtime without throwing an error")
+    func testBedtimePrediction() throws {
+        let wakeUpTime = ContentView.defaultWakeTime
+        let desiredSleep = 8.0
+        let coffees = 1
+        
+        let predictedBedtime = SleepPredictor.calculateBedtime(wakeUp: wakeUpTime, sleepAmount: desiredSleep, coffeeAmount: coffees)
+        
+        #expect(predictedBedtime != nil)
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: predictedBedtime!)
+        #expect(components.hour == 22)
+        #expect(components.minute == 38)
     }
 
 }
