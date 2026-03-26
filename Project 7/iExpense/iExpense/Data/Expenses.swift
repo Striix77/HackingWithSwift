54ef76d6266b342ae3f825a9d2a5cfeaaaf9d9cf
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 15:08:06 +0200

    Make expenses list initialize from UserDefaults

--- Project 7/iExpense/iExpense/Data/Expenses.swift
+++ Project 7/iExpense/iExpense/Data/Expenses.swift
@@ -15,5 +15,15 @@ class Expenses {
             }
         }
     }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
 
