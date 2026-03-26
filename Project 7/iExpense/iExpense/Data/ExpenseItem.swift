925e8cdef8f3e2a6d832802c98720e5ef6fcd3d8
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 14:01:16 +0200

    Create struct for Expense items

---
+++ Project 7/iExpense/iExpense/Data/ExpenseItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Freak on 26.03.2026.
+//
+import Foundation
+
+struct ExpenseItem{
+    let name: String
+    let type: String
+    let amount: Double
+}
