4c03f2414208a2784382fa945212d78a9d666b9d
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 14:47:55 +0200

    Create AddView view for adding new expenses

---
+++ Project 7/iExpense/iExpense/Views/AddView.swift
@@ -0,0 +1,44 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Freak on 26.03.2026.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+
+    let types = ["Business", "Personal"]
+
+    var expenses: Expenses
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField(
+                    "Amount",
+                    value: $amount,
+                    format: .currency(code: "USD")
+                )
+                .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
