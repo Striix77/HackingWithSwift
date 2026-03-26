ce04e9af8664fc2cde9acf433c537d74497e4168
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 14:11:53 +0200

    Modify to conform to view model

--- Project 7/iExpense/iExpense/ContentView.swift
+++ Project 7/iExpense/iExpense/ContentView.swift
@@ -8,30 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var expenses = Expenses()
+    @State var viewModel = ExpensesViewModel()
     var body: some View {
-        NavigationStack{
-            ExpensesListView(expenses:expenses)
+        NavigationStack {
+            ExpensesListView(
+                expenses: viewModel.expenses,
+                removeItems: viewModel.removeItems
+            )
         }
     }
 }
 
 struct ExpensesListView: View {
     let expenses: Expenses
+    var removeItems: (IndexSet) -> Void
     var body: some View {
         List {
-            ForEach(expenses.items, id:\.name ){ item in
+            ForEach(expenses.items, id: \.name) { item in
                 Text(item.name)
             }
+            .onDelete(perform: removeItems)
         }
         .navigationTitle("iExpense")
-        .toolbar{
-            Button("Add expense", systemImage: "plus"){
-                let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+        .toolbar {
+            Button("Add expense", systemImage: "plus") {
+                let expense = ExpenseItem(
+                    name: "Test",
+                    type: "Personal",
+                    amount: 5
+                )
                 expenses.items.append(expense)
             }
         }
     }
+
 }
 
 #Preview {
