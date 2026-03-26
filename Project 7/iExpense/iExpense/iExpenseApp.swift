42b5bb57000e62515363aa0a52b94bc9129b0c45
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 12:52:56 +0200

    Create project iExpense

---
+++ Project 7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Freak on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
