73ab1ca4168739fde2fa89fd7bddbbd0ebb5a62e
Erik M <muzsi.erik@agilefreaks.com>
2026-03-26 15:57:30 +0200

    Add filtering by type in the expense list

--- Project 7/iExpense/iExpense/Views/ExpensesListView.swift
+++ Project 7/iExpense/iExpense/Views/ExpensesListView.swift
@@ -8,11 +8,24 @@ import SwiftUI
 
 struct ExpensesListView: View {
     @Binding var showingAddExpense: Bool
+    @State var expenseType = ""
+    
     let expenses: Expenses
     var removeItems: (IndexSet) -> Void
+    var expenseTypes = ["All", "Personal", "Business"]
+    
+    var filteredExpenses: [ExpenseItem] {
+        if expenseType == "" || expenseType == "All" {
+            return expenses.items
+        }
+        return expenses.items.filter{
+            $0.type == expenseType
+        }
+    }
+    
     var body: some View {
         List {
-            ForEach(expenses.items) { item in
+            ForEach(filteredExpenses) { item in
                 HStack {
                     VStack(alignment: .leading) {
                         Text(item.name)
@@ -37,9 +50,24 @@ struct ExpensesListView: View {
                     showingAddExpense = true
                 }
             }
+            
+            ToolbarItem(placement:.topBarTrailing) {
+                Menu{
+                    Picker("", selection:$expenseType){
+                        ForEach(expenseTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } label:{
+                    Text(expenseType == "" ? "Filter" : expenseType)
+                        .foregroundStyle(.gray)
+                }
+            }
         }
         .sheet(isPresented: $showingAddExpense) {
             AddView(expenses: expenses)
         }
     }
+    
+    
 }
