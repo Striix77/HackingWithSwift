be3ca4c99f235a6b715d6159bde9ebcb1efb726f
Erik M <muzsi.erik@agilefreaks.com>
2026-03-27 12:04:19 +0200

    Create formatted launch date in Mission

--- Project 8/Moonshot/Moonshot/Data/Mission.swift
+++ Project 8/Moonshot/Moonshot/Data/Mission.swift
@@ -13,7 +13,7 @@ struct Mission: Codable, Identifiable{
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
@@ -22,5 +22,8 @@ struct Mission: Codable, Identifiable{
     }
     var image: String {
         "apollo\(id)"
+    }
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
