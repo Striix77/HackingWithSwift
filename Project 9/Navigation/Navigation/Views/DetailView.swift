ca5c8bd38f1dd051789a8dc559605c3716f5eff1
Erik M <muzsi.erik@agilefreaks.com>
2026-03-30 15:12:21 +0300

    Create detail view

---
+++ Project 9/Navigation/Navigation/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Navigation
+//
+//  Created by Freak on 30.03.2026.
+//
+import SwiftUI
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: NavigationPath
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
+    }
+    
+}
