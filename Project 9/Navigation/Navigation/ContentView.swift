283c87c4e2e35ce7bab07f3c151425560f7c4345
Erik M <muzsi.erik@agilefreaks.com>
2026-03-30 15:59:01 +0300

    Add a list inside the string list navigation paths

--- Project 9/Navigation/Navigation/ContentView.swift
+++ Project 9/Navigation/Navigation/ContentView.swift
@@ -10,23 +10,31 @@ import SwiftUI
 struct ContentView: View {
     @State private var pathStore = PathStore()
 
-        var body: some View {
-            NavigationStack(path: $pathStore.path) {
-                DetailView(number: 0, path: $pathStore.path)
-                    .navigationDestination(for: Int.self) { i in
-                        DetailView(number: i, path: $pathStore.path)
-                    }
-                stringNavigationList
-            }
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $pathStore.path)
+                }
+            stringNavigationList
         }
-    
-    private var stringNavigationList: some View{
+    }
+
+    private var stringNavigationList: some View {
         List(0..<100) { i in
-                NavigationLink("Select \(i)", value: String(i+7))
+            NavigationLink("Select \(i)", value: String(i + 7))
+        }
+        .navigationDestination(for: String.self) { selection in
+            VStack {
+                List(0..<100) { number in
+                    Text(
+                        "\(number). You selected \(selection). That's a string btw."
+                    )
+                }
+
             }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected \(selection). That's a string btw.")
-            }
+            .navigationTitle("\(selection)")
+        }
     }
 }
 
--- Project 9/Navigation/Navigation/Views/DetailView.swift
+++ Project 9/Navigation/Navigation/Views/DetailView.swift
@@ -12,12 +12,12 @@ struct DetailView: View {
 
     var body: some View {
         NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
             .toolbar {
                 Button("Home") {
                     path = NavigationPath()
                 }
             }
+            .navigationTitle("Number: \(number)")
     }
     
 }
