2f6e22a73ae4965f20b7fb0d5047f14f8bbaec88
Erik M <muzsi.erik@agilefreaks.com>
2026-03-31 11:46:44 +0300

    Create CheckoutView

---
+++ Project 10/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Freak on 31.03.2026.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
