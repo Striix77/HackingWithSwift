3654234f4b523a9fe1e29df792d8af0cead0e399
Erik M <muzsi.erik@agilefreaks.com>
2026-03-31 11:29:01 +0300

    Create Project 10: CupcakeCorner

---
+++ Project 10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Freak on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project 10/CupcakeCorner/CupcakeCornerTests/CupcakeCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerTests.swift
+//  CupcakeCornerTests
+//
+//  Created by Freak on 31.03.2026.
+//
+
+import Testing
+@testable import CupcakeCorner
+
+struct CupcakeCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
