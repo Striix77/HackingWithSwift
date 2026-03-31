89598906749b3404c9396a64d65d2d4a7d442b10
Erik M <muzsi.erik@agilefreaks.com>
2026-03-31 12:17:23 +0300

    Extract checkout header into its own view

--- Project 10/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
+++ Project 10/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
@@ -13,25 +13,35 @@ struct CheckoutView: View {
     var body: some View {
         ScrollView {
             VStack {
-                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(height: 233)
+                CheckoutHeaderView(order: order)
 
-                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
-                    .font(.title)
-
-                Button("Place Order", action: { })
+                Button("Place Order", action: {})
                     .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+    }
+
+}
+
+private struct CheckoutHeaderView: View {
+    var order: Order
+    var body: some View {
+        AsyncImage(
+            url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"),
+            scale: 3
+        ) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(height: 233)
+        Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+            .font(.title)
     }
 }
 
